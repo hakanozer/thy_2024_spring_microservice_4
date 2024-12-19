ee209a30f413b089c992eaadb0f46a85084f1a8e
Hakan ÖZER <hakanozer02@gmail.com>
2024-12-19 11:00:12 +0300

    Customer and Role

---
+++ Api Gateway/src/main/java/com/works/configs/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.works.configs;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return
+                http
+                .csrf( csrf -> csrf.disable() )
+                .formLogin( formLogin -> formLogin.disable() )
+                .build();
+
+    }
+
+}
---
+++ Api Gateway/src/main/java/com/works/entities/Customer.java
@@ -0,0 +1,23 @@
+package com.works.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cid;
+
+    private String username;
+    private String password;
+    private Boolean enable;
+
+    @ManyToMany
+    private List<Role> roles;
+
+}
