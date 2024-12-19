89f225a039dad2890f7979f6e28f6b83f5edb5bc
Hakan ÖZER <hakanozer02@gmail.com>
2024-12-19 14:08:59 +0300

    APi Gateway

--- Api Gateway/src/main/java/com/works/configs/SecurityConfig.java
+++ Api Gateway/src/main/java/com/works/configs/SecurityConfig.java
@@ -1,24 +1,49 @@
 package com.works.configs;
 
+import com.works.services.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    final CustomerService customerService;
+    final PasswordEncoder passwordEncoder;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return
                 http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests( req ->
+                    req
+                    .requestMatchers("/product/**").hasRole("product")
+                    .requestMatchers("/basket/**").hasRole("basket")
+                    .requestMatchers("/customer/**").permitAll()
+                )
                 .csrf( csrf -> csrf.disable() )
                 .formLogin( formLogin -> formLogin.disable() )
                 .build();
 
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(customerService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        return daoAuthenticationProvider;
+    }
+
+
 
 }
 /*
